de805b672d4e4389c375e0fdaf632f991395af2e
chouhanvikash <96488608+chouhanvikash@users.noreply.github.com>
2025-09-26 09:06:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	let res="";
+	let char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
+	for(i=0;i<l;i++)
+		{
+		res+=char_list.charAt(
+			Math.floor(Math.random()*l))	;
+		}
+	return res;
 }
 
 // Do not change the code below.
